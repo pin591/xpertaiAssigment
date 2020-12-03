36a336adb3e29fc14203adc5c6f8528fd91a2ce7
Ana <anarebollo@xpertai.com>
2020-12-03 18:45:49 +0100

    Initial Commit

---
+++ xpertaiAssigment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xpertaiAssigment
+//
+//  Created by Ana Rebollo Pin on 03/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
